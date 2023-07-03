390c66ecfdb984ac19b332028be9e1ed9302a9f3
Steven Gutierrez <stevogbmv@gmail.com>
2023-07-02 19:17:13 -0700

    Backend: update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,8 @@ class ApplicationController < Sinatra::Base
     categories.to_json(include: {products:{include: {reviews:{only: [:rating, :name, :created_at]}}}})
   end
 
-  get "/products" do 
-    products = Product.all
-    products.to_json
-  end
-
   get '/products/:id' do
     products = Product.find(params[:id])
-
-    # include associated reviews in the JSON response
     products.to_json(include: :reviews)
   end
 
@@ -23,16 +16,19 @@ class ApplicationController < Sinatra::Base
     reviews.to_json
   end 
 
+  post '/reviews' do 
+    reviews = Review.create(
+      product_id: params[:product_id],
+      name: params[:name],
+      rating: params[:rating],
+    )
+    reviews.to_json 
+  end
+
+
   get "/cart_products" do 
     cartProducts = CartProduct.all 
     cartProducts.to_json(include: :product)
-  end
-
-  
-
-  get "/cart" do 
-    cart = ShoppingCart.find(1)
-    cart.to_json(include: { cart_products: { include: :product} })
   end
 
   post "/cart_products" do 
